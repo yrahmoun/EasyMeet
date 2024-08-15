543ea951905b75645b809a583e3523dc94898b07
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-08-15 21:00:44 +0100

    setting up user authentication routes for the backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 dotenv.config();
+connectDB();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("api is running")
 });
 
+app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 
-app.listen(5000, console.log(`server is up and running on port ${PORT}!`));
+app.listen(PORT, console.log(`server is up and running on port ${PORT}!`));
