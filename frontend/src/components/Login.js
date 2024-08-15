d97607d8ad244b31acf285f492a722c05ec3eb22
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-08-15 21:51:01 +0100

    add functionality to the sign up button: it stores data in the database. add functionality o the login button: checks the info if they belong o an existing user before granting access

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,12 +4,68 @@ import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { Button } from "@chakra-ui/button";
 import { useState } from "react";
+import axios from "axios";
+import { useToast } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+  const toast = useToast();
+  const history = useHistory();
+  const [loading, setLoading] = useState(false);
+
+  const submitHandler = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
 
   return (
     <VStack spacing="10px">
@@ -45,8 +101,8 @@ const Login = () => {
         bg="#333"
         _hover={{ bg: '#555' }}
         style={{ marginTop: 15 }}
-        //onClick={submitHandler}
-        //isLoading={loading}
+        onClick={submitHandler}
+        isLoading={loading}
       >
         Login
       </Button>
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -4,6 +4,9 @@ import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { Button } from "@chakra-ui/button";
 import { useState } from "react";
+import { useToast } from "@chakra-ui/toast";
+import { useHistory } from "react-router";
+import axios from "axios";
 
 const Signup = () => {
   const [name, setName] = useState();
@@ -11,7 +14,70 @@ const Signup = () => {
   const [password, setPassword] = useState();
   const [confirmpassword, setConfirmpassword] = useState();
   const [show, setShow] = useState(false);
+  const [pic, setPic] = useState();
+  const toast = useToast();
+  const history = useHistory();
   const handleClick = () => setShow(!show);
+
+  const submitHandler = async () => {
+    if (!name || !email || !password || !confirmpassword) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    if (password !== confirmpassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    console.log(name, email, password, pic);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user",
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      console.log(data);
+      toast({
+        title: "Registration Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+  };
 
   return (
     <VStack spacing="5px">
@@ -74,6 +140,7 @@ const Signup = () => {
         _hover={{ bg: '#555' }}
         width="100%"
         style={{ marginTop: 15 }}
+        onClick={submitHandler}
       >
         Sign Up
       </Button>
