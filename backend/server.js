d898d128277f5059f3401eb589ab57534b28fdb6
Yasser Rahmouni <rahmouniyasser77@gmail.com>
2024-08-22 15:14:55 +0100

    building the up for production and setting it up for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 app = express();
 dotenv.config();
@@ -12,13 +13,25 @@ connectDB();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send("api is running")
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+
+const __dirname1 = path.resolve();
+
+if (process.env.APP_STATE === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
